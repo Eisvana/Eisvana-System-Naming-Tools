91d6409796d11828256bbda5127f6e2a112a8290
Lenni <guentherhanslphdyt@gmail.com>
2026-02-15 13:31:12 +0100

    use aliases to make linter happy

--- src/logic/coordGlyphConvert.ts
+++ src/logic/coordGlyphConvert.ts
@@ -1,9 +1,9 @@
-import { GalacticCoordinate, PortalCode } from '@nmscd/coordinate-conversion';
+import { GalacticCoordinate as galacticCoordinate, PortalCode as portalCode } from '@nmscd/coordinate-conversion';
 
 // coords to glyphs
 export const coords2Glyphs = (coords: string): string =>
-  GalacticCoordinate({ code: coords }).toGlyph().value?.code ?? coords;
+  galacticCoordinate({ code: coords }).toGlyph().value?.code ?? coords;
 
 // glyphs to coords
 export const glyphs2Coords = (glyphs: string) =>
-  PortalCode({ code: glyphs }).toGalacticCoordinates().value?.code ?? glyphs;
+  portalCode({ code: glyphs }).toGalacticCoordinates().value?.code ?? glyphs;
